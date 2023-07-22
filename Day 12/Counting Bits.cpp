db262e6a25f24ad9dfa2abbcc768d18e01590a78
Sneha920 <84587260+Sneha920@users.noreply.github.com>
2023-07-22 15:15:21 +0530

    Update Counting Bits.cpp

--- Day 12/Counting Bits.cpp
+++ Day 12/Counting Bits.cpp
@@ -9,3 +9,9 @@ public:
         return ans;
     }
 };
+// The logic Behind "Why for even numbers the number of set bits would be same as its half and for odd numbers it would be +1" ??.
+
+// 1.All odd numbers would have their LSB (the rightmost bit) set. The reason being that no bit other than 0th bit will contribute an addition of odd number; all other powers of 2 will add to even.
+// example: (3 -> 0011) (5 -> 0101) (7 -> 0111). So when we do a right shift >> 1 (divide by 2), we will always lose the rightmost bit. So in order to compensate for the lost bit, we add 1.
+
+// 2.For similar reasons, we see that even numbers would never have LSB set. Example. (2 -> 0010), (4 -> 0100), (6 -> 0110). Even when we right shift >> 1 (divide by 2), we see that there would be no loss of set bits. So we do not need to add 1.
